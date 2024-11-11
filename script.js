c8a9cdb6c5a8b26b72181157d958c614addc4591
DaiRodri <daianarodri@gmail.com>
2024-11-11 03:23:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ function carregar(){
     var img = document.getElementById("imagem")
     var data = new Date()
     var hora = data.getHours()
-      msg.innerHTML = `Agora são ${hora} horas.`
-    if( hora >= 0 && hora < 12){
+    var minuto = data.getMinutes()
+      msg.innerHTML = `Agora são ${hora} horas:${minuto} minutos`
+    
+      if( hora >= 0 && hora < 12){
         //msg.innerHTML = `bom dia`
         img.src = "manha.jpg"
     }else if ( hora >= 12 && hora < 18){
